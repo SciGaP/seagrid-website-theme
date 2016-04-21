6372dcb53ce3caf723a3a6fcaea34787c6374d3e
spamidig <pamidigs@iu.edu>
2016-04-21 14:36:26 -0500

    announce

--- partials/template.blade.php
+++ partials/template.blade.php
@@ -1,4 +1,4 @@
-<div class="col-md-3 text-center breathing-space seagrid-info">
+<div class="col-md-12 text-center well">
         <a target="_blank" href="https://scigap.atlassian.net/wiki/display/SEAGrid/XSEDE+Workshop-Albany+State+University+2016"> <h4> XSEDE Workshop At Albany State University, 22-23 April, 2016 </h4> </a>
 </div>
 <div class="col-md-12 hero-unit">
