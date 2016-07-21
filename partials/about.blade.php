aa26ad15703ff42111e18c71334a733af59a6b4b
spamidig <pamidigs@iu.edu>
2016-07-21 06:16:18 -0500

    updated citation

--- partials/about.blade.php
+++ partials/about.blade.php
@@ -31,11 +31,12 @@
 			<h3>Cite:</h3>
 			<h4>Citing SEAGrid in academic papers helps us show impact. If you use SEAGrid in your research please help by including the following citation information and acknowledgement in your paper.</h4>
 			<p>SEAGrid (<a href="http://www.seagrid.org">http:www.seagrid.org</a>)[*] is acknowledged for computational resources and services for the selected results used in this publication. </p>
-			<p>* (a) N. Shen, Y. Fan, S. Pamidighantam, E-Science Infrastructures for Molecular Modeling and Parametrization, Journal of Computational Science 5 (2014), pp. 576-589; DOI information: 10.1016/j.jocs.2014.01.005</p>
-			<p> (b) R. Dooley, K. Milfeld, C. Guiang, S. Pamidighantam, G. Allen, From Proposal to Production: Lessons Learned Developing the Computational Chemistry Grid Cyberinfrastructure, Journal of Grid Computing, 2006, 4, 195-208. </p>
-			<p> (c) R. Dooley, G. Allen, and S. Pamidighantam. "Computational Chemistry Grid: Production Cyberinfrastructure for Computational Chemistry". Proceedings of the 13th Annual Mardi Gras Conference, Baton Rouge, LA, Feb. 2005, pg 83. </p>
-			<p> (d) K. Milfeld, C. Guiang, S. Pamidighantam, J. Giuliani. Cluster Computing through an Application-oriented Computational Chemistry Grid. Proceedings of the 2005 Linux Clusters: The HPC Revolution, Apr. 2005. </p>
-			<p> (e) This work used the Extreme Science and Engineering Discovery Environment (XSEDE), which is supported by National Science Foundation grant number OCI-1053575.</p>
+			<p> * (a) S. Pamidighantama, S Nakandala, E. Abeysinghe, C Wimalasena, S. Rathnayakae, S. Marru, M. Pierce, Community Science Exemplars in SEAGrid Science Gateway: Apache Airavata Based Implementation of Advanced Infrastructure, Procedia Computer Science Volume 80, 2016, Pages 1927–1939 </p>
+			<p> (b) N. Shen, Y. Fan, S. Pamidighantam, E-Science Infrastructures for Molecular Modeling and Parametrization, Journal of Computational Science 5 (2014), pp. 576-589; DOI information: 10.1016/j.jocs.2014.01.005</p>
+			<p> (c) R. Dooley, K. Milfeld, C. Guiang, S. Pamidighantam, G. Allen, From Proposal to Production: Lessons Learned Developing the Computational Chemistry Grid Cyberinfrastructure, Journal of Grid Computing, 2006, 4, 195-208. </p>
+			<p> (d) R. Dooley, G. Allen, and S. Pamidighantam. "Computational Chemistry Grid: Production Cyberinfrastructure for Computational Chemistry". Proceedings of the 13th Annual Mardi Gras Conference, Baton Rouge, LA, Feb. 2005, pg 83. </p>
+			<p> (e) K. Milfeld, C. Guiang, S. Pamidighantam, J. Giuliani. Cluster Computing through an Application-oriented Computational Chemistry Grid. Proceedings of the 2005 Linux Clusters: The HPC Revolution, Apr. 2005. </p>
+			<p> (f) This work used the Extreme Science and Engineering Discovery Environment (XSEDE), which is supported by National Science Foundation grant number OCI-1053575.</p>
 	 		<a target="_blank" href="{{URL::to('/')}}/themes/{{Session::get('theme')}}/assets/app/BibTex.txt">Get BibTex</a>
 		</div>
 	</div>
