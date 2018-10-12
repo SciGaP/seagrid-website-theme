de1a22e4ee989cdd69ad3d92d58285a4544d68b8
spamidig <pamidigs@iu.edu>
2018-10-12 13:41:06 -0400

    Updated publications

--- partials/publications.blade.php
+++ partials/publications.blade.php
@@ -9,13 +9,28 @@
 <ul style="line-height:1.6em;">
 
        <h3> 2018 </h3>
+     
+      <li> J. Heo, D. Dumett Torres, and P. K. Jain. Unconventional Long-Range Order in Copper Selenide Nanocrystals. Submitted to Chem. Mater., 2018. </em></li>
 
+     <li> J. Heo, D. Dumett Torres, P. Banerjee, and P. K. Jain. In-Situ TEM Observation of an Order-Disorder Transition in Superionic Conductor Cu2Se. Submitted to Nat. Mater., 2018. </em></li>
+ 
+      <li> Dumett Torres, Daniel, Jain, Prashant K. &nbsp;<strong>  Strain Stabilization of Superionicity in Copper and Lithium Selenides </strong>&nbsp; <em> J. Phys. Chem. Lett., 2018, 9 (6), pp 1200–1205, DOI: 10.1021/acs.jpclett.8b00236 </em></li>
+
+       <li> Leila Tabrizi, Duy Quang Dao, Thuy An Vu, "Experimental and Theoretical Evaluation on Antioxidant Activity of Copper (II) Complex Based on Lidocaine and Ibuprofen Amide-Phenanthroline Agents", Inorganic Chemistry (2018), (Under revision with manuscript ID ic-2018-02153m). </em></li>
+      
+       
+      <li> Jalal Z. A. Laloo  Nandini Savoo  Nassirah Laloo  Lydia Rhyman  Ponnadurai Ramasam, &nbsp;<strong>  ExcelAutomat 1.3: Fragment analysis based on the distortion/interaction‐activation strain model </strong>&nbsp; <em>  Journal of Computational Chemistry 2018, Pages 1-6, https://doi.org/10.1002/jcc.25546 </em></li>
+     
+      <li> Dimer Products from Ligand-Exchange Reactions During Thermal Atomic Layer Etching" Andrew Cavanagh' Joel Clancey' Sandeep Sharma' Steven George, 18th International Conference on Atomic Layer Deposition, Incheon, South Korea, July 2018. </em></li>
+ 
       <li> Susmita Mandal, Mousumi Das, Piu Das, Amalesh Samanta, Ray J. Butcher, Manan Saha, Ibrahim A. Alswaidan, Lydia Rhyman, Ponnadurai Ramasami, Nitis Chandra Saha,&nbsp;<strong> Synthesis, characterization, DFT and antimicrobial studies of transition metal ion complexes of a new schiff base ligand, 5-methylpyrazole-3yl-N-(2́-hydroxyphenylamine)methyleneimine, (MPzOAP),
 </strong>&nbsp; <em> Journal of Molecular Structure, 2018, , ISSN 0022-2860, https://doi.org/10.1016/j.molstruc.2018.09.095. </em></li> 
  
+       <li> Thi Chinh Ngo, Nguyen Thi Hau, Duy Quang Dao, "Radical Scavenging Activity of Natural-based Cassaine Diterpenoid Amides and Amines", Journal of Chemical Information and Modeling (2018), (Under review with manuscript ID ci-2018-00644c) </em></li>
+
        <li> Dhanashree Hallooman, Mar Ríos-Gutiérrez, Lydia Rhyman, Ibrahim A. Alswaidand, Luis R. Domingo and Ponnadurai Ramasami &nbsp;<strong> DFT exploration of [3 + 2] cycloaddition reaction of 1H-phosphorinium-3-olate and 1-methylphosphorinium-3-olate with methyl methacrylate </strong>&nbsp; <em> RSC Adv., 2018, 8, 27406-27416, DOI: 10.1039/C8RA04703K </em></li>
   
-       <li> Minu G. Bhowon, Sabina Jhaumeer Laulloo, Eric C. Hosten, Muzammil M. Khodabaccus, Lydia Rhyman, Ponnadurai Ramasami </strong>&nbsp; <em> Synthesis, spectroscopic, biological and DFT studies of new t-butyl substituted salicylaldimines having disulfide moiety </strong>&nbsp; <em> Journal of Molecular Structure, Volume 1175, 2019, Pages 13-23, ISSN 0022-2860, https://doi.org/10.1016/j.molstruc.2018.07.086.</em></li> 
+       <li> Minu G. Bhowon, Sabina Jhaumeer Laulloo, Eric C. Hosten, Muzammil M. Khodabaccus, Lydia Rhyman, Ponnadurai Ramasami &nbsp; <strong> Synthesis, spectroscopic, biological and DFT studies of new t-butyl substituted salicylaldimines having disulfide moiety </strong>&nbsp; <em> Journal of Molecular Structure, Volume 1175, 2019, Pages 13-23, ISSN 0022-2860, https://doi.org/10.1016/j.molstruc.2018.07.086.</em></li> 
 
        <li> Siddheshwar D. Jadhav, Ibrahim A. Alswaidan, Lydia Rhyman, Ponnadurai Ramasami, Nagaiyan Sekar &nbsp;<strong> Effect of methoxy group on NLOphoric properties of fluorescent 7-arylstyryl-2-methoxyphenylimidazo[1,2-a]pyridine - Solvatochromic and computational method </strong>&nbsp; <em> Journal of Molecular Structure, Volume 1173, 2018, Pages 349-365, ISSN 0022-2860, https://doi.org/10.1016/j.molstruc.2018.07.010.</em></li>
 
